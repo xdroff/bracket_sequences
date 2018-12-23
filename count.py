1f9d8e18e5e62c916d8a51781095bf0f7e878c38
droff <droff@inbox.ru>
2018-12-24 00:25:30 +0300

    Сorrect spelling error

--- count.py
+++ count.py
@@ -1,6 +1,6 @@
-def main(braket_sequences: str):
+def main(bracket_sequences: str):
     count = 0
-    for x in braket_sequences:
+    for x in bracket_sequences:
         if count == -1:
             break
         if x == '(':
@@ -8,9 +8,9 @@ def main(braket_sequences: str):
         if x == ')':
             count -= 1
     if count == 0:
-        print(f'Bracket sequences: "{braket_sequences}" is correct')
+        print(f'Bracket sequences: "{bracket_sequences}" is correct')
     else:
-        print(f'Bracket sequences: "{braket_sequences}" is not correct')
+        print(f'Bracket sequences: "{bracket_sequences}" is not correct')
 
 if __name__ == "__main__":
     main('())')
